5ea006e8d62e28d319dd5491db2ba1b6ea1210d9
Kedasha Kerr <kedashakerr@northwesternmutual.com>
2021-11-16 10:49:27 -0600

    initial setup

---
+++ react-client/src/App.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <Button color="danger">Danger!</Button>
+  );
+}
+
+export default App;
