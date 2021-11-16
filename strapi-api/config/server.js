a47b257432bb88a681c3d63619a0eba22bcb8feb
Kedasha Kerr <kedashakerr@northwesternmutual.com>
2021-11-15 21:57:24 -0600

    initial setup

---
+++ strapi-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '12b688465d65e4dd9edc2e332218efb8'),
+    },
+  },
+});
